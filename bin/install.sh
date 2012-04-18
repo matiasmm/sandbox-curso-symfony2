51dd3da3516273122c5d5e358821fda73449c2b1
Matias <matiasmmontenegro@yahoo.com.ar>
2012-04-18 20:47:37 -0300

    new install

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+echo 'Base de Datos. Usuario: '
+read BD_USER
+echo 'Base de Datos. Password: '
+read BD_PASSWORD
+
+
 if [ ! -f app/config/parameters.yml ]
 then
     cp app/config/parameters.yml-dist app/config/parameters.yml
 fi
 
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+
 chmod -R ugo+rwx app/cache
 chmod -R ugo+rwx app/logs
-mysqladmin --force drop cupon -u root
-mysqladmin --force create cupon -u root
-mysql -u root cupon < database-dump.sql
+mysqladmin --force drop --user=$BD_USER --password=$BD_PASSWORD cupon 
+mysqladmin --force create --user=$BD_USER --password=$BD_PASSWORD cupon 
+mysql --user=$BD_USER --password=$BD_PASSWORD cupon < database-dump.sql
